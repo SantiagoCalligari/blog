e32aebece993decc3f2be395554dca81fc50f59d
Santiago Calligari <89709491+SantiagoCalligari@users.noreply.github.com>
2023-12-24 18:55:21 -0300

    ammend

---
+++ src/components/Article.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let size: number;
+  export let date: string;
+  export let name: string;
+</script>
+
+<div class="row mx-4 inline-block">
+  <ul class="list-inline">
+    <li class="list-inline-item text-danger">-rwxr-x-r-x</li>
+    <li class="list-inline-item text-success">root root</li>
+    <li class="list-inline-item text-warning">{size}</li>
+    <li class="list-inline-item text-info">{date}</li>
+    <li class="list-inline-item text-warning">
+      <a href="/{name}">{name}</a>
+    </li>
+  </ul>
+</div>
